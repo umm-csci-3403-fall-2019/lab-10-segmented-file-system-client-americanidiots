839f9e46e3acd77de5de76ca587360858bcc1461
hill1588 <hill1588@morris.umn.edu>
2019-12-03 11:10:44 -0600

    Start to port and socket set-up
    began to code the client side so that we can retreive the files.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,35 @@
 package segmentedfilesystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
+import javax.sound.sampled.Port;
+
 public class Main {
+
+        public static final int PORT_NUMBER = 6014;
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Main client = new Main();
+        client.start();
     }
 
+    private void start() throws IOException {
+        DatagramSocket socket = new DatagramSocket(PORT_NUMBER);
+        byte[] buf = new byte[1028];
+
+        try {
+            in = new BufferedReader(new FileReader("one-liners.txt"));
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+        }   
+        catch (FileNotFoundException e){
+            System.err.println("Couldn't open quote file.  Serving time instead.");
+        }
+    }  
+        socket.close();
+    }
 }
