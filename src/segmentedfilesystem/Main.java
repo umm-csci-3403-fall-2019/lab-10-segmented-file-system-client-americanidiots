d092d759da12bdb06c4aa7e4d42216bea2ed6f89
hill1588 <hill1588@morris.umn.edu>
2019-12-10 10:22:30 -0600

    change to main
    chagned to resemble QuoteClient class

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -3,6 +3,7 @@ package segmentedfilesystem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.Buffer;
 
@@ -12,24 +13,20 @@ public class Main {
 
         public static final int PORT_NUMBER = 6014;
     
-    public static void main(String[] args) throws IOException {
-        Main client = new Main();
-        client.start();
-    }
+    public static void main(String[] args) {
 
-    private void start() throws IOException {
-        DatagramSocket socket = new DatagramSocket(PORT_NUMBER);
-        byte[] buf = new byte[1028];
-
-        try {
-            in = new BufferedReader(new FileReader("one-liners.txt"));
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-        }   
-        catch (FileNotFoundException e){
-            System.err.println("Couldn't open quote file.  Serving time instead.");
+        if (args.length != 1) {
+            System.out.println("Usage: java QuoteClient <hostname>");
+            return;
         }
-    }  
-        socket.close();
+        DatagramSocket socket = new DatagramSocket();
     }
+
+    private void start() {
+        // DatagramSocket socket = new DatagramSocket(PORT_NUMBER);
+        byte[] buf = new byte[1028];
+        InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, PORT_NUMBER);
+        socket.send(packet);
+  }
 }
