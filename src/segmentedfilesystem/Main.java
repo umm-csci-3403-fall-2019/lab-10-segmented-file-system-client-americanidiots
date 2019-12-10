714e64f270825a81e68d43aeb2b29536982df14c
Trent Russell Merkins <merki002@umm9020-h2vhx12.morris.umn.edu>
2019-12-10 11:36:48 -0600

    This was stuff I worked on

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,7 +1,10 @@
 package segmentedfilesystem;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Socket;
 import java.nio.Buffer;
@@ -10,7 +13,7 @@ import javax.sound.sampled.Port;
 
 public class Main {
 
-        public static final int PORT_NUMBER = 6014;
+        public static final int PORT_NUMBER = ;
     
     public static void main(String[] args) throws IOException {
         Main client = new Main();
@@ -22,15 +25,19 @@ public class Main {
         byte[] buf = new byte[1028];
 
         try {
-            in = new BufferedReader(new FileReader("one-liners.txt"));
+            BufferedReader in = new BufferedReader(new FileReader("one-liners.txt"));
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
+            addPacket(packet);
+            socket.send(packet);
+            
         }   
         catch (FileNotFoundException e){
             System.err.println("Couldn't open quote file.  Serving time instead.");
         }
     }  
-        writeFile();
+
+    writeFile();
         isDone();
         socket.close();
     }
---
+++ src/segmentedfilesystem/Packets.java
@@ -0,0 +1,38 @@
+package segmentedfilesystem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
+import javax.sound.sampled.Port;
+import javax.xml.crypto.Data;
+
+public class Packets {
+
+    public static void packetType(DatagramPacket packet){
+
+        Object packetType;
+        Object DataPacket;
+        Object HeaderPacket;
+
+		if(status byte odd){
+            packetType = DataPacket;
+        }
+        if(second bit is also 1){
+            packetType = DataPacket;
+            isDone();
+        }
+        else{
+            packetType = HeaderPacket;
+        }
+    }
+    public static void addPacket(HeaderPacket p) {
+        
+    }
+    public static void addPacket(DataPacket p) {
+        
+    }
+}
