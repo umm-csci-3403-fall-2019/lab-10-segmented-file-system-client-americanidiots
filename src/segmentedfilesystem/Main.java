8a776fe2779a6b6e7253d0bb06a434f42fa559ae
hill1588 <hill1588@morris.umn.edu>
2019-12-10 11:31:43 -0600

    add maps to assit with sorting of packets
    base structure of statements to help with understanding of how we will go about seperating the packets. addition of maps to help put data packets where they should go accordingly.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -2,35 +2,98 @@ package segmentedfilesystem;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.*;
 import java.nio.Buffer;
+import java.util.Map;
 
 import javax.sound.sampled.Port;
 
 public class Main {
 
         public static final int PORT_NUMBER = 6014;
+        public static final String ADDRESS = "csci-4409.morris.umn.edu ";
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Map<Integer, Integer> map0 = new Map<>();
+        Map<Integer, Integer> map1 = new Map<>();
+        Map<Integer, Integer> map2 = new Map<>();
+
+
 
         if (args.length != 1) {
             System.out.println("Usage: java QuoteClient <hostname>");
             return;
         }
-        DatagramSocket socket = new DatagramSocket();
-    }  
-        writeFile();
-        isDone();
-        socket.close();
+        DatagramSocket socket = new DatagramSocket(PORT_NUMBER);
     }
+    // writeFile();
+    // isDone();
+    // socket.close();
+    // }
 
     private void start() {
-        // DatagramSocket socket = new DatagramSocket(PORT_NUMBER);
-        byte[] buf = new byte[1028];
-        InetAddress address = InetAddress.getByName(args[0]);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, PORT_NUMBER);
+        byte[] buf = new byte[1028]; // can be used like an array
+        // ([array], [length of array], [server address], [port to connect to])
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, ADDRESS, PORT_NUMBER);
         socket.send(packet);
+
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Quote of the Moment: " + received);
+
+        // sort as we go
+
+        // total number of packets Identified last packet number
+        int packetnum0 =-1;
+        int packetnum1 =-1;
+        int packetnum2 =-1;
+
+        //This is packet status Header or Data
+        int x = buf[0];
+
+        //This says what file the packet belongs to
+        int y = buf[1];
+
+        //This identifies the packet 
+        if(x % 2 == 0) {
+            //means its a header packet
+            //place header into the correct file
+            if(y==0){
+                map0.put(packet);
+            }
+            if(y==1){
+                map1.put(packet);
+            }
+            if(y==2){
+                map2.put(packet);
+            }
+        }
+        else {
+            //means this is a data packet
+            if(x % 4 == 1) {
+                // int 
+                //last data packet
+                if(y==0){
+                    packetnum0 = lastdatapacketnum;
+                    map0.put(packet);
+                }
+                if(y==1){
+                    packetnum1 = lastdatapacketnum;
+                    map1.put(packet);
+                }
+                if(y==2){
+                    packetnum2 = lastdatapacketnum;
+                    map2.put(packet);
+                }
+
+                //pass last two bytes to the isDone method to check if there are that many
+                //files, if so then file is compelete, else continue
+            }
+            else {
+                //not the last packet, place where it needs to go in the (file) map
+            }
+        }
   }
 }
+
